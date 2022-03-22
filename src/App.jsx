ee6a337cd7d7f63cb99744bfc502c9ca360a8010
Fachri Maulana <fachryfachry1997@gmail.com>
2022-03-22 15:15:33 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import registerServiceWorker from './serviceWorkerRegistration';
+import registerServiceWorker from "./serviceWorkerRegistration";
 
 import "./index.scss";
 import Admin from "./layouts/Admin";
@@ -16,30 +16,22 @@ function FallbackComponent() {
 
 const myFallback = <FallbackComponent />;
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
 }
 
 const App = () => (
-  // <div className="mt-10 text-3xl mx-auto max-w-6xl">
-  //   <FirebaseMessaging />
-  //   <Sentry.ErrorBoundary fallback={myFallback} showDialog>
-  //     <CardTable />
-  //   </Sentry.ErrorBoundary>
-
-  // </div>
   <>
-
     <BrowserRouter>
       <Switch>
         {/* add routes with layouts */}
-        <Route path="/admin" component={Admin} />
+        <Route path="/" component={Admin} />
         <Route path="/auth" component={Auth} />
         {/* <Route path="/social" component={Home} /> */}
         {/* add redirect for first page */}
-        <Redirect from="*" to="/admin" />
+        {/* <Redirect from="*" to="/admin" /> */}
       </Switch>
-    </BrowserRouter>,
+    </BrowserRouter>
   </>
 );
 
