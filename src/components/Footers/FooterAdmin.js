aca2a0177ff1875da39d360f4e39165d8027dd15
Fachri Maulana <fachryfachry1997@gmail.com>
2022-03-25 08:54:38 +0700

    fix

--- src/components/Footers/FooterAdmin.js
+++ src/components/Footers/FooterAdmin.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Home from "social/Home";
 
 export default function FooterAdmin() {
   return (
@@ -10,7 +9,7 @@ export default function FooterAdmin() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4">
               <div className="text-sm text-slate-500 font-semibold py-1 text-center md:text-left">
-                Copyright © {new Date().getFullYear()} <Home />
+                Copyright © {new Date().getFullYear()}{" "}
               </div>
             </div>
           </div>
