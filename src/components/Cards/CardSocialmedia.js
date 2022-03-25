8116ecdba4c8aff6c81cfe24dfc66e6ef8186031
Fachri Maulana <fachryfachry1997@gmail.com>
2022-03-25 09:08:35 +0700

    fix

---
+++ src/components/Cards/CardSocialmedia.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "social/Home";
+
+function CardSocialmedia() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default CardSocialmedia;
